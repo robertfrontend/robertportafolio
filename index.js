a84dd9bfdae5af9fc4a85184daff79d0d26482ba
Robert Romero <enanow41romero@gmail.com>
2019-10-18 22:07:25 -0400

    Version 1.0 de mi portafolio

---
+++ index.js
@@ -0,0 +1,23 @@
+//seccion del portafolio oculto
+
+document.getElementById("btn-vermas").addEventListener("click",function(){
+  
+document.getElementById("seccion-ocultos").classList.toggle("is-active")
+});
+
+
+// seccion de sobre mi
+
+document.getElementById('conoceme').addEventListener("click",function(){
+
+document.getElementById("div-desc-soy").classList.toggle("active-desc")
+});
+
+
+//memnu
+
+document.getElementById('bt-menu').addEventListener("click",function(){
+
+document.getElementById("div-menu").classList.toggle("activador-menu")
+});
+
