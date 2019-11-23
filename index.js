21101794b770c66e3765926c6a869872266ac30c
Robert Romero <enanow41romero@gmail.com>
2019-11-23 13:15:23 -0400

    Modo dark y modo light, super hermoso

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//funcion anonima 
+
+(function(){
+
+});
+
+
 //seccion del portafolio oculto
 
 document.getElementById("btn-vermas").addEventListener("click",function(){
@@ -22,3 +29,28 @@ document.getElementById("div-menu").classList.toggle("activador-menu")
 });
 
 
+//modo oscuro
+document.getElementById("cambiar-oscuro").addEventListener("click",function(e){
+    var isActive = e.target.checked;
+    var switchTitle = document.getElementsByClassName("switch-title")[0];
+
+    if (isActive) {
+        document.getElementsByTagName("body")[0].classList.add("dark");
+        switchTitle.innerHTML = "White";
+    }else{
+        document.getElementsByTagName("body")[0].classList.remove("dark");
+        switchTitle.innerHTML = "Dark";
+    }
+
+    // document.getElementsByClassName(body).classList.toggle("is-active")
+});
+
+
+// trabajar con el scroll de la pagina 
+window.addEventListener("scroll", function(event){
+    if (this.scrollY > 50){
+        document.getElementById("switch-container").classList.add("flotar");
+    }else{
+        document.getElementById("switch-container").classList.remove("flotar");
+    }
+});
